659c5ae80cba51297d8b9410125680858c3d7290
anusreepromod <anusreepi@gmail.com>
2021-06-11 14:59:28 +0530

    version 1

---
+++ firstapp/views.py
@@ -0,0 +1,10 @@
+from django.http.response import HttpResponse
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def hello(request):
+    return HttpResponse("Hello world")
+
+def htm(request):
+    return render(request,'sample.html')
